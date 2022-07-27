fed7eba8845cf285afd61618fc5066d0fa1e3813
William Can <wilenrike@gmail.com>
2022-07-26 23:44:27 -0600

    fix: Api file

--- client/src/Config/Api.js
+++ client/src/Config/Api.js
@@ -1,6 +1,4 @@
 const BACKEND_HOST = import.meta.env.VITE_APP_BACKEND_URI || 'http://192.168.1.112:5000';
-console.log("Import", import.meta.env.VITE_APP_BACKEND_URI);
-console.log("cosntat", BACKEND_HOST)
 
 export default {
     HOST: BACKEND_HOST,
